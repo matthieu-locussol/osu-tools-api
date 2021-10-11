8b0ff2807cf9304b9b1fbf43abc55991cc63a54a
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-10-11 17:43:42 +0200

    fix: console log stderr on error in execute

--- src/utils/execute.ts
+++ src/utils/execute.ts
@@ -2,8 +2,9 @@ import { exec } from 'child_process';
 
 export const execute = (command: string): Promise<unknown> => {
    const promise = new Promise((resolve, reject) => {
-      exec(command, (error, stdout) => {
+      exec(command, (error, stdout, stderr) => {
          if (error !== null) {
+            console.error('Error: ', stderr);
             reject(error);
          } else {
             resolve(stdout);
