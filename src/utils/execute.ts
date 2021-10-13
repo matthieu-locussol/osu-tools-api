19cd18137794cf45639e599925b48911c3e221cf
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-10-13 20:27:43 +0200

    fix: fixed error throwing on beatmap cache-miss

--- src/utils/execute.ts
+++ src/utils/execute.ts
@@ -5,6 +5,12 @@ export const execute = (command: string): Promise<string> => {
       exec(command, (error, stdout) => {
          if (error !== null) {
             reject(error);
+         }
+         // When there is a cache-miss, osu-tools downloads the map and output a line in stdout.
+         // We need to remove this line to parse only the JSON output.
+         else if (stdout.startsWith('Downloading')) {
+            const output = stdout.split('\n').slice(1).join('\n');
+            resolve(output);
          } else {
             resolve(stdout);
          }
