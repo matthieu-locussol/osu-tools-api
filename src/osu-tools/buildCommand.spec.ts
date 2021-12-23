cfb9da98dc59ef5837a224f0d9224819e6d36727
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-12-23 11:12:45 +0100

    fix: updated dotnet path default value

--- src/osu-tools/buildCommand.spec.ts
+++ src/osu-tools/buildCommand.spec.ts
@@ -3,7 +3,9 @@ import { buildCommand } from './buildCommand';
 describe(__filename, () => {
    test('buildCommand', () => {
       expect(buildCommand('simulate', 'path', 'config', 'args')).toEqual(
-         `${process.env.DOTNET_PATH}dotnet run --project path --configuration config -- simulate args --json`,
+         `${
+            process.env.DOTNET_PATH || ''
+         }dotnet run --project path --configuration config -- simulate args --json`,
       );
    });
 });
